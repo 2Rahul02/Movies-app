39c61af548f1cd82d6145b572150cf1bfbd29178
Rahul Sharma <2rahul.sharma9.2002@gmail.com>
2021-11-09 23:11:00 +0530

    Movie-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
+import Navbar from './components/navbar';
+import Banner from './components/banner';
+import Movie from './components/movies';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+
 import './App.css';
+import Favourite from './components/favourite';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( 
+        
+   <Router>
+   <Navbar/>
+   <Switch>
+     <Route path='/' exact render={(props)=>(
+       <>
+         <Banner {...props}/>
+         <Movie {...props}/>
+       </>
+     )}/>
+     <Route path='/favourites' component={Favourite} />
+   </Switch>
+ </Router>
+    );
 }
 
 export default App;
